fea02a73524012b42c1c9d5b991a8edff4e1d276
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2023-05-21 10:41:14 +0600

    complete sa soft cafe

--- src/components/Modele/Modele.jsx
+++ src/components/Modele/Modele.jsx
@@ -4,13 +4,19 @@ import Products from '../Products/Products';
 
 const Modele = () => {
     const [ products, setProducts]= useState([]);
-    //const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([])
 
     useEffect(()=>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     }, [])
+
+    const handelAddToCart = (product) => {
+        const newCart =[...cart, product];
+        setCart(newCart);    
+}
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
@@ -18,6 +24,7 @@ const Modele = () => {
                     products.map(product => <Products
                     key={product.id}
                     product ={product}
+                    handelAddToCart ={handelAddToCart}
                     ></Products>)
                 }
             </div>
@@ -26,10 +33,20 @@ const Modele = () => {
                 <div className='summary'>
                     <h3>Bookmarked Blogs: {} 8</h3>
                     <div className='add'>
-                        <p>hello</p>
-                        <p>hello</p>
-                        <p>hello</p>
-                        <p>hello</p>
+                        <p>Master Microsoft Power Platform <br/> and Become an In-Demand!</p>
+                        <p>How to get your first job self-taught <br /> programmer in top company</p>
+                        <p>Master Microsoft Power Platform <br/> and Become an In-Demand!</p>
+                        <p>How to get your first job self-taught <br /> programmer in top company</p>
+                        <p>Master Microsoft Power Platform <br/> and Become an In-Demand!</p>
+                        <p>How to get your first job self-taught <br /> programmer in top company</p>
+                        <p>Master Microsoft Power Platform <br/> and Become an In-Demand!</p>
+                        <p>Master Microsoft Power Platform <br/> and Become an In-Demand!</p>
+                        <p>How to get your first job self-taught <br /> programmer in top company</p>
+                        <p>Master Microsoft Power Platform <br/> and Become an In-Demand!</p>
+                        <p>Master Microsoft Power Platform <br/> and Become an In-Demand!</p>
+                        <p>How to get your first job self-taught <br /> programmer in top company</p>
+                        <p>Master Microsoft Power Platform <br/> and Become an In-Demand!</p>
+                        
                     </div>
                     
                 </div>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './Products.css'
 
 const Products = (props) => {
-    const {img, user, icon, name, title, button} = props.product;
+    
+    const {img, user, icon, name, title, button,} = props.product;
+    const handelAddToCart = props.handelAddToCart;
 
+    
     return (
         <div className='product'>
             <img src={img} alt=""/>
@@ -15,13 +18,12 @@ const Products = (props) => {
             <div className='icon'>
                 <p>05minread</p>
                 <img src={icon} alt="" />
+                
             </div>
         </div>
-        <h2>{title}</h2>
+         <h2 >{title}</h2>
         <p>#beganer #programming</p>
         <h4>{button}</h4>
-            
-            
         </div>
     );
 };
